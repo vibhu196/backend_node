04a97bcbd7c735ad5a7aa7fff33536b71859b5bd
Vibhu <thakurvibhu196@gmail.com>
2025-05-04 15:46:28 +0530

    auth api and sockets

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // Import individual route modules
 const authRoutes = require('./authRoutes');
 const chatRoutes = require('./chatRoutes');
-const todoRoutes = require('./todoRoutes'); // If any
+const todoRoutes = require('./todoRoutes'); 
 
 // Use them under appropriate base paths
 router.use('/auth', authRoutes);         // /api/auth/...
